536a3606ddd4b4f340ca534c16da529d71b8f5b2
kerwei <kerwei84@gmail.com>
2020-09-27 00:00:33 +0800

    Use unittest instead of unittest2

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,4 +1,4 @@
-import unittest2
+import unittest
 
 from consecutivesum import whilecount
 from distinctslice import distinct_slice
@@ -7,11 +7,11 @@ from maxproduct import maxproduct
 from nondivisor import solve
 
 
-class TestConsecSum(unittest2.TestCase):
+class TestConsecSum(unittest.TestCase):
     def test_simple(self):
         self.assertEqual(whilecount(103), [[51,52]])
 
-class TestLookAndSay(unittest2.TestCase):
+class TestLookAndSay(unittest.TestCase):
     def test_negative(self):
         with self.assertRaises(ValueError): lookandsay(-1)
 
@@ -21,14 +21,14 @@ class TestLookAndSay(unittest2.TestCase):
     def test_simple(self):
         self.assertEqual(lookandsay(10), '13211311123113112211')
 
-class TestNonDivisor(unittest2.TestCase):
+class TestNonDivisor(unittest.TestCase):
     def test_simple(self):
         self.assertEqual(solve([3, 1, 2, 3, 6]), [2, 4, 3, 2, 0])
 
     def test_small(self):
         self.assertEqual(solve([2, 4]), [1, 0])
 
-class TestMaxProduct(unittest2.TestCase):
+class TestMaxProduct(unittest.TestCase):
     def test_small_even(self):
         self.assertEqual(maxproduct(4), 4)
 
@@ -41,7 +41,7 @@ class TestMaxProduct(unittest2.TestCase):
     def test_odd(self):
         self.assertEqual(maxproduct(9), 27)
 
-class TestDistinctSlice(unittest2.TestCase):
+class TestDistinctSlice(unittest.TestCase):
     def test_simple(self) -> None:
         A = [3, 4, 5, 5, 2]
         self.assertEqual(distinct_slice(6, A), 9)
@@ -57,3 +57,7 @@ class TestDistinctSlice(unittest2.TestCase):
     def test_alternate(self) -> None:
         A = [2, 3, 2, 3]
         self.assertEqual(distinct_slice(3, A), 7)
+
+class TestMinAbsSum(unittest.TestCase):
+    def test_simple(self):
+        pass
