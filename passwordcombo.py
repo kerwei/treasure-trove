5f8d3843254798255c4250ed2b29d7543e1d6ae8
kerwei <kerwei84@gmail.com>
2021-02-03 14:52:58 +0800

    Expand description in docstring. Add comments to major blocks

--- passwordcombo.py
+++ passwordcombo.py
@@ -5,9 +5,13 @@ from typing import List
 
 def password(A: List, N: int) -> List:
     """
-    Return all possible combinations of a password from list. Where two or
-    more element words are combined, they are concatenated by an underscore
+    Return all possible combination of passwords of a specified length from
+    list. Where two or more element words are combined, they are concatenated
+    by an underscore. For example, 
+    given A = ['one', 'A', 'list', 'of', 'random', 'words'], 
+    password(A, 5) should return ['one_A', 'A_one', 'words', 'A_A_A', 'of_of']
     """
+    # Allocate list members into buckets of length
     dct = {i:[] for i in range(1, max([len(a) for a in A]) + 1)}
     for a in A:
         dct[len(a)] += [a]
@@ -20,6 +24,8 @@ def password(A: List, N: int) -> List:
     if dct.get(2):
         combo[2] = [tuple([2])]
 
+    # Apply dynamic programming to discover the possible configurations to
+    # achieve the desired pass word length. Use list position indexing
     for i in range(3, N + 1):
         ctr = []
         for j in range(1, (i//2) + 1):
@@ -32,6 +38,9 @@ def password(A: List, N: int) -> List:
         else:
             combo[i] = list(itertools.chain(*ctr))
 
+    # For all configurations that are applicable to a given password length,
+    # generate all password outputs through a product operation between all
+    # members
     tracker = {}
     output = []
     for cmb in set(combo[-1]):
